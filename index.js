9e161436afe66c8b4db5ce0b6c5263adef8838bc
wanjiruth <njoki.ruth.wanjiku@gmail.com>
2024-03-23 16:02:12 +0300

    HOHA

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+// returnFirstTwoDrivers function
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2);
+  }
+  
+  // returnLastTwoDrivers function
+  const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2);
+  }
+  
+  // selectingDrivers array
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // createFareMultiplier function
+  const createFareMultiplier = (multiplier) => {
+    return (fare) => fare * multiplier;
+  }
+  
+  // fareDoubler function
+  const fareDoubler = createFareMultiplier(2);
+  
+  // fareTripler function
+  const fareTripler = createFareMultiplier(3);
+  
+  // selectDifferentDrivers function
+  const selectDifferentDrivers = (drivers, selectorFunction) => {
+    return selectorFunction(drivers);
+  }
+  
+  module.exports = {
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+    selectingDrivers,
+    createFareMultiplier,
+    fareDoubler,
+    fareTripler,
+    selectDifferentDrivers
+  };
+  
